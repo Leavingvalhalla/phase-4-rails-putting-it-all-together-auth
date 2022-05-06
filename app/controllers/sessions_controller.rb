ede06bbfd55c1bcffdf4f7105548b9c520dacfd2
Zach Lefler <leaving.valhalla@gmail.com>
2022-05-05 17:21:51 -0700

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: {error: "Invalid username/password"}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        user = User.find_by(id: session[:user_id])
+        if user
+        session.delete :user_id
+        else
+            render json: {errors: user.errors.full_messages}, status: :unauthorized
+        end
+    end
+end
